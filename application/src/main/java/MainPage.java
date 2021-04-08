88e3f3a6737c726dea51fccc044ac1cede016623
AlexanderArgyriou <Alexander.K.Argyriou@gmail.com>
2021-04-08 23:02:54 +0300

    test servlet1
    (cherry picked from commit 797f2d25bbd2d296a90a608c7f36f9f71172f25a)

--- application/src/main/java/MainPage.java
+++ application/src/main/java/MainPage.java
@@ -10,5 +10,6 @@ public class MainPage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/test.jsp").forward(req, resp);
+        req.getCookies();
     }
 }
