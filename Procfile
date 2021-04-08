9697385dcf0c00450a06febf3845de3cb2a2b55e
AlexanderArgyriou <Alexander.K.Argyriou@gmail.com>
2021-04-08 22:56:53 +0300

    test servlet
    (cherry picked from commit c7543022ccbaf42dabb96bf3d8c6071f489645af)

--- Procfile
+++
@@ -1,3 +0,0 @@
-buildpacks:clear
-ps:scale web=1
-war:deploy target/application-1.0-SNAPSHOT.war
--- application/src/main/java/MainPage.java
+++ application/src/main/java/MainPage.java
@@ -1,11 +1,14 @@
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@Path("/")
-public class MainPage {
-
-    @GET
-    public String mainPage() {
-        return "index.jsp";
+@WebServlet("/test")
+public class MainPage extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/test.jsp").forward(req, resp);
     }
 }
